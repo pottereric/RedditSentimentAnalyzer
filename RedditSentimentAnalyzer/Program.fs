acd516f61809c44acd6a960d3d41e4ae2201e5f0
Eric Potter <pottereric@hotmail.com>
2018-04-03 22:13:07 -0400

    Print off the negative comments from the prominent language subreddits.

--- RedditSentimentAnalyzer/Program.fs
+++ RedditSentimentAnalyzer/Program.fs
@@ -12,6 +12,12 @@ let convertTitlesToInputs titles =
         let text, score = t
         "en", i.ToString(), text)
 
+type RedditTitleSentiment = 
+    { Votes: int
+      Title: string
+      Sentiment: float option
+    }
+
 let getSentimentAndTitlesForsubreddit subreddit sort =
     let titles = Reddit.GetTitles subreddit sort
     let inputs = titles |> convertTitlesToInputs
@@ -19,9 +25,27 @@ let getSentimentAndTitlesForsubreddit subreddit sort =
     let client = FognitiveServices.Text.Client.create Config.subscriptionKey Config.azureRegion
     let result = FognitiveServices.Text.Client.sentimentAnalysis client inputs
 
-    let sentiments = result.Documents |> Seq.map(fun d -> d.Id, d.Score) |> List.ofSeq
-    List.zip3 titles inputs sentiments
+    let sentiments = result.Documents |> Seq.map(fun d -> d.Id, (d.Score |> Option.ofNullable)) |> List.ofSeq
+    let combinedLists = List.zip3 titles inputs sentiments
+    combinedLists |> List.map(fun item -> 
+        let (text, score),(lang, id, title), (id2, sent) = item
+        {Votes = score; Title = text; Sentiment = sent}
+    )
 
+let printNegativeTitles redditSentimentData =
+    redditSentimentData 
+    |> List.where(fun item -> item.Sentiment.Value < 0.5)
+    |> List.sortBy (fun item -> item.Sentiment)
+    |> List.iter (fun item -> printfn "(%d) - (%f) %s" item.Votes item.Sentiment.Value item.Title )
+
+let printNegativeTitlesForLanguageSubreddits _ =
+    Reddit.programmingSubreddits
+    |> List.iter (fun subreddit ->
+        printfn "== %s ==" subreddit
+        getSentimentAndTitlesForsubreddit subreddit "top"
+        |> printNegativeTitles
+        printfn ""
+    )
 
 [<EntryPoint>]
 let main argv = 
@@ -29,13 +53,12 @@ let main argv =
     //Reddit.GetNewProgrammingTitles() |> printTitles
 
     //let redditSentimentData = getSentimentAndTitlesForsubreddit "programming" "top"
-    let redditSentimentData = getSentimentAndTitlesForsubreddit "programming" "new"
-    redditSentimentData |> List.iter (fun item ->
-        let (text, score),(lang, id, title), (id2, sent) = item
-        //printfn "(%d) - (%f) [%s,%s] %s : %s" score sent.Value id id2 text title
-        printfn "(%d) - (%f) %s" score sent.Value title
-    )
+    //let redditSentimentData = getSentimentAndTitlesForsubreddit "programming" "new"
+    //let redditSentimentData = getSentimentAndTitlesForsubreddit "programming" "new"
 
+    //printNegativeTitles redditSentimentData
+
+    printNegativeTitlesForLanguageSubreddits()
 
 
     System.Console.ReadKey() |> ignore
--- RedditSentimentAnalyzer/Reddit.fs
+++ RedditSentimentAnalyzer/Reddit.fs
@@ -21,3 +21,49 @@ let GetHotProgrammingTitles _ =
 let GetNewProgrammingTitles _ =
     GetTitles "programming" "new"
 
+let programmingSubreddits = 
+    [
+        "python"; //210k users
+        "javascript"; //190k users
+        "java"; //80.3k users
+        "cpp"; //58.3k users
+        "php"; //57.7k users
+        "csharp"; //50.8k users
+        "golang"; //38.8k users
+        "ruby"; //38.0k users
+        "rust"; //32.6k users
+        "c_programming"; //32.0k users
+        "swift"; //31.2k users
+        "haskell"; //30.5k users
+        "sql"; //21.1k users
+        "scala"; //15.7k users
+        "rstats"; //15.2k users
+        "latex"; //13.7k users
+        "matlab"; //13.7k users
+        "clojure"; //13.2k users
+        "lisp"; //12.5k users
+        "perl"; //10.8k users
+        "elixir"; //9.3k users
+        "kotlin"; //8.0k users
+        "erlang"; //6.1k users
+        "elm"; //5.9k users
+        "lua"; //5.6k users
+        "objectivec"; //5.6k users
+        "asm"; //4.6k users
+        "scheme"; //4.5k users
+        "fsharp"; //4.2k users
+        "ocaml"; //4.0k users
+        "julia"; //3.8k users
+        "mathematica"; //3.7k users
+        "visualbasic"; //3.6k users
+        "racket"; //2.8k users
+        "coffeescript"; //2.4k users
+        "dartlang"; //2.3k users
+        "d_language"; //2.1k users
+        "gpgpu"; //2.0k users
+        "shell"; //2.0k users
+        "groovy"; //1.8k users
+        "prolog"; //1.8k users
+        "fortran"; //1.6k users
+    ]
+
